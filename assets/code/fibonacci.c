2a62fef654886114458f38ec39a141adc33316bc
Yves Chevallier <yves.chevallier@heig-vd.ch>
2022-08-07 12:43:49 +0200

    Fix fibonacci code

--- assets/code/fibonacci.c
+++ assets/code/fibonacci.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     int n;
-    printf("Combien de termes voulez-vous ? : ");
-    scanf("%d", &n);
+    if (argc < 2) {
+        printf("Combien de termes voulez-vous ? : ");
+        scanf("%d", &n);
+    } else {
+        n = atoi(argv[1]);
+    }
 
     int t[2] = {0, 1};
     for (size_t i = 1; i <= n; i++)
     {
-        printf("%d, ", t1);
+        printf("%d ", t[1]);
         int next_term = t[0] + t[1];
-        t[1] = t[0];
+        t[0] = t[1];
         t[1] = next_term;
     }
-    printf("\n");
+    puts("");
 }
