3eb94e706a1196b5cf52d40ae5290977fabdec3d
Oberson-Antoine <antoine.oberson@yahoo.fr>
2024-03-11 19:55:06 +0100

    docker for arm and x64

---
+++ .devcontainer/install-amd64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget=/usr/bin/wget
+
+wget https://github.com/jgraph/drawio-desktop/releases/download/v24.0.4/drawio-amd64-24.0.4.deb
+dpkg -i drawio-amd64*
---
+++ .devcontainer/install-arm64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget=/usr/bin/wget
+
+wget https://github.com/jgraph/drawio-desktop/releases/download/v24.0.4/drawio-arm64-24.0.4.deb 
+dpkg -i drawio-arm64* 
